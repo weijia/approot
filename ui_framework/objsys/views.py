5b1e1ec89477c23eb3c9a687cfbea9e838121ac3
Richard <richardwangwang@gmail.com>
2013-07-14 17:06:33 +0800

    Fixed timezone import issue.

--- ui_framework/objsys/views.py
+++ ui_framework/objsys/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.conf import settings
 import os
+from time import timezone
 from models import UfsObj, get_ufs_obj_from_ufs_url
 from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
