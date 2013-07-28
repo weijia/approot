d219222aecb563d1fc013dff31a8cc81bc4ed2c7
Richard <richardwangwang@gmail.com>
2013-07-28 15:41:27 +0800

    Added diagrams.

--- libs/services/external_app/start_ext_app.bat
+++ libs/services/external_app/start_ext_app.bat
@@ -1,6 +1,6 @@
 call activate.bat
 ext_svr
-manage syncdb --noinput
+if exist syncdb_needed.txt manage syncdb --noinput
 rem python libs\services\apps\tube_logging_service.py --input "ufs_test_tube"
 service_starter
 tornado.bat
