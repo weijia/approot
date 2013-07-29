35ce88c534f0ef03778f52f28af21dc9fa727aff
Richard <richardwangwang@gmail.com>
2013-07-29 22:17:12 +0800

    list view OK.

--- libs/mobile/ftp_sync.py
+++ libs/mobile/ftp_sync.py
@@ -14,19 +14,24 @@ Builder.load_string("""
 """)
 
 
-class ListViewModal(ModalView):
+class DataItem(SelectableDataItem):
+    def __init__(self, text='', is_selected=False):
+        self.text = text
+        self.is_selected = is_selected
 
+
+class ListViewModal(ModalView):
     list_view = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         super(ListViewModal, self).__init__(**kwargs)
 
-        items = self.list_view.data
-        item = SelectableDataItem("ftp://a:b@c.com")
+        items = self.list_view.adapter.data
+        item = u"ftp://a:b@c.com"
         items.append(item)
 
-class MainView(GridLayout):
 
+class MainView(GridLayout):
     def __init__(self, **kwargs):
         kwargs['cols'] = 1
         super(MainView, self).__init__(**kwargs)
@@ -34,8 +39,6 @@ class MainView(GridLayout):
         listview_modal = ListViewModal()
 
         self.add_widget(listview_modal)
-
-
 
 
 if __name__ == '__main__':
