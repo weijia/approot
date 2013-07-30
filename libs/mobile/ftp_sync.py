17f93b0a4b53c640fba5c5b6674fbb05408023cc
richard <richardwangwang@gmail.com>
2013-07-30 12:09:17 +0800

    Use box layout.

--- libs/mobile/ftp_sync.py
+++ libs/mobile/ftp_sync.py
@@ -5,17 +5,18 @@ kivy.require('1.3.0')
 #import time
 
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
+#from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 
 class FtpSyncApp(App):
     def build(self):
-        parent = FloatLayout(size=(500,500))
+        parent = BoxLayout()
         stopButtonHeight = 90
-        self.stopButton = Button(text='Stop', pos_hint={'right':1}, size_hint=(None,None), height=stopButtonHeight)
+        self.stopButton = Button(text='Stop', height=stopButtonHeight)
         parent.add_widget(self.stopButton)
-        #self.startButton = Button(text='Start', pos_hint={'right':1}, size_hint=(None,None), height=stopButtonHeight)
-        #parent.add_widget(self.startButton)
+        self.startButton = Button(text='Start', height=stopButtonHeight)
+        parent.add_widget(self.startButton)
         return parent
 
 
