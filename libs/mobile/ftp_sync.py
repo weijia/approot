a06b6ee11558318943a2341821cb3179223718cd
richard <richardwangwang@gmail.com>
2013-07-30 11:05:16 +0800

    Start from a new point. Every commit should run here.

--- libs/mobile/ftp_sync.py
+++ libs/mobile/ftp_sync.py
@@ -1,44 +1,15 @@
-from kivy.uix.modalview import ModalView
-from kivy.uix.listview import ListView
-from kivy.uix.gridlayout import GridLayout
-from kivy.lang import Builder
-from kivy.adapters.models import SelectableDataItem
-from kivy.properties import ObjectProperty
+import kivy
+kivy.require('1.3.0')
 
-Builder.load_string("""
-<ListViewModal>:
-    list_view: list_view
-    size_hint: None, None
-    ListView:
-        id: list_view
-""")
+#import math
+#import time
+
+from kivy.app import App
 
 
-class DataItem(SelectableDataItem):
-    def __init__(self, text='', is_selected=False):
-        self.text = text
-        self.is_selected = is_selected
+class FtpSyncApp(App):
+    pass
 
 
-class ListViewModal(ModalView):
-    list_view = ObjectProperty(None)
-
-    def __init__(self, **kwargs):
-        super(ListViewModal, self).__init__(**kwargs)
-
-        items = self.list_view.adapter.data
-        item = u"ftp://a:b@c.com"
-        items.append(item)
-
-
-class MainView(GridLayout):
-    def __init__(self, **kwargs):
-        kwargs['cols'] = 1
-        super(MainView, self).__init__(**kwargs)
-        listview_modal = ListViewModal()
-        self.add_widget(listview_modal)
-
-
-if __name__ == '__main__':
-    from kivy.base import runTouchApp
-    runTouchApp(MainView(width=800))
+if __name__ in ('__main__', '__android__'):
+    FtpSyncApp().run()
