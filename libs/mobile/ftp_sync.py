15cb041b8b3bce914efc3d722ceee0e7cb353dc0
richard <richardwangwang@gmail.com>
2013-07-30 11:19:40 +0800

    Added button OK.

--- libs/mobile/ftp_sync.py
+++ libs/mobile/ftp_sync.py
@@ -5,10 +5,18 @@ kivy.require('1.3.0')
 #import time
 
 from kivy.app import App
-
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
 
 class FtpSyncApp(App):
-    pass
+    def build(self):
+        parent = FloatLayout(size=(500,500))
+        stopButtonHeight = 90
+        self.stopButton = Button(text='Stop', pos_hint={'right':1}, size_hint=(None,None), height=stopButtonHeight)
+        parent.add_widget(self.stopButton)
+        #self.startButton = Button(text='Start', pos_hint={'right':1}, size_hint=(None,None), height=stopButtonHeight)
+        #parent.add_widget(self.startButton)
+        return parent
 
 
 if __name__ in ('__main__', '__android__'):
