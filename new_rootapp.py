63f6c2af976b602a039d1189c97a892956b0117e
Richard <richardwangwang@gmail.com>
2013-09-06 11:31:46 +0800

    Added close callback.

--- new_rootapp.py
+++ new_rootapp.py
@@ -4,15 +4,20 @@ from libs.utils.filetools import findAppInProduct
 import configuration
 
 
+def stop_postgresql():
+    os.system(findAppInProduct("postgresql_stop"))
+
+
 def main():
     try:
-        Iconizer().execute({"ext_svr": [findAppInProduct("new_ext_svr")]})
+        i = Iconizer()
+        i.add_close_listener(stop_postgresql)
+        i.execute({"ext_svr": [findAppInProduct("new_ext_svr")]})
     except (KeyboardInterrupt, SystemExit):
         raise
-
     #print "stopping database"
-    os.system("D:\\codes\\python\\codes\\ufs_django\\approot\\libs\\services\\external_app\\postgresql_stop.bat")
 
 
 if __name__ == '__main__':
     main()
+    #os.system(findAppInProduct("postgresql_stop"))
