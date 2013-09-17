f6e6d704a8ef429d5603d25ff7c9448435b1149d
Richard <richardwangwang@gmail.com>
2013-09-17 16:06:38 +0800

    Adding obj downloader.

---
+++ libs/services/apps/obj_downloader.py
@@ -0,0 +1,9 @@
+
+
+
+#Generate target path
+#approot/../exported/hostname/timestamp.txt: include data and all server timestamps
+{"exported": [{"name1":"data1"}], "server_timestamps":{"server1-timestamp":1000000, "server2-timestamp"}}
+#Find the latest dump file
+#Check the latest timestamp for a server
+
