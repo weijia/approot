d7152e9c51148abeb5d602e929c8db594feea6de
Richard <richardwangwang@gmail.com>
2013-09-21 23:30:35 +0800

    Added new codes for obj download.

---
+++ libs/datetime_storage/datetime_folders.py
@@ -0,0 +1,46 @@
+import os
+import datetime
+import time
+from libs.utils.misc import ensure_dir
+
+
+class PathIsNotDirException(Exception): pass
+
+
+class DateTimeFolder(object):
+    def __init__(self, root_folder):
+        if not os.path.isdir(root_folder):
+            raise PathIsNotDirException
+        self.root_folder = root_folder
+
+    def enumerate_from_latest(self):
+         year_folder_list = os.listdir(self.root_folder)
+         year_folder_list.sort(reverse=True)
+         for year_folder in year_folder_list:
+             year_folder_full_path = os.path.join(self.root_folder, year_folder)
+             if os.path.isdir(year_folder_full_path):
+                 month_folder_list = os.listdir(year_folder_full_path)
+                 month_folder_list.sort(reverse=True)
+                 for month_folder_name in month_folder_list:
+                     month_folder_full_path = os.path.join(year_folder_full_path, month_folder_name)
+                     if os.path.isdir(month_folder_full_path):
+                         filenames = os.listdir(month_folder_full_path)
+                         filenames.sort(reverse=True)
+                         for filename in filenames:
+                             yield os.path.join(month_folder_full_path, filename)
+
+    def get_folder_for_today(self):
+        now = datetime.datetime.utcnow()
+        return os.path.join(self.root_folder, "%s/%s/%s" % (now.year, now.month, time.time()))
+
+
+
+d = DateTimeFolder("d:/")
+cnt = 0
+for i in d.enumerate_from_latest():
+    print i
+    cnt += 1
+    if cnt > 6:
+        break
+
+print d.get_folder_for_today()
--- libs/services/apps/obj_downloader.py
+++ libs/services/apps/obj_downloader.py
@@ -1,21 +1,102 @@
-
-
+import json
+import os
+import urlparse
+import sys
+import argparse
 
 #Generate target path
-#approot/../exported/hostname/timestamp.txt: include data and all server timestamps
+#approot/../exported/hostname/year/month/timestamp.txt: include data and all server timestamps
+#approot/../exported/hostname/2013/09/1234234234.12.txt: include data and all server timestamps
+#Every dump file contains dumped duration for a server. App create a map for those durations and only dump
+#the durations that is not in the dump folder.
+
 
 #Find the latest dump file
-{"exported": [{"name1":"data1"}], "server_timestamps":{"server1-timestamp":1000000, "server2-timestamp"}}
+import datetime
+import jsonpickle
+from timeslice.TimeSlice import TimeSlice, TimeSet
+from libs.datetime_storage.datetime_folders import DateTimeFolder
+from libs.utils.misc import ensure_dir
+
+dump_file_format = {"exported": [{"name1": "data1"}], "server_timestamps":
+    {"server1-timestamp": 1000000, "server2-timestamp": 20000000}}
 #Check the latest timestamp for a server
 
-        parser = argparse.ArgumentParser()
-        #print self.param_dict
-        ############################
-        parser.add_argument("--server", help="server name to download from")
+parser = argparse.ArgumentParser()
+#print self.param_dict
+############################
+parser.add_argument("--server", help="server name to download from")
 
-        print sys.argv
-        #print parser
-        args = vars(parser.parse_args())
-        return args
+print sys.argv
+#print parser
+args = vars(parser.parse_args())
 
-urllib
+print args['server']
+p = urlparse.urlparse(args['server'])
+print p.netloc
+
+g_dump_root_folder = "d:/tmp/dumproot/"
+ensure_dir(g_dump_root_folder)
+g_dump_state_folder = "d:/tmp/dumpstate/"
+ensure_dir(g_dump_state_folder)
+
+root = DateTimeFolder(g_dump_root_folder)
+
+offset = 0
+
+
+class NewStyleObjectTimeSlice(TimeSlice, object):
+    pass
+
+
+class NewStyleObjectTimeSet(TimeSet, object):
+    pass
+
+
+###################################
+# Load current state file from folder
+###################################
+state_file_path = os.path.join(g_dump_state_folder, "state.txt")
+
+if os.path.exists(state_file_path):
+    state_file = open(state_file_path, "r")
+    slices = jsonpickle.decode(state_file.read())
+    state_file.close()
+
+
+def get_last_timestamp_for_server(server_loc):
+    for last_dump_file_path in root.enumerate_from_latest():
+        item = json.load(last_dump_file_path)
+        try:
+            return item["server_timestamps"][server_loc]
+        except KeyError:
+            pass
+
+for folder_for_host in os.listdir(g_dump_root_folder):
+    host_dump_folder_full_path = os.path.join(g_dump_root_folder, folder_for_host)
+
+
+time_set = NewStyleObjectTimeSet()
+
+
+def dump_data_for_time_slice(time_slice):
+    pass
+
+
+#Check if the first item on server is dumped
+if 0 == len(time_set):
+    start = datetime.datetime(datetime.MINYEAR, 1, 1)
+
+
+if (0 == len(time_set)) or (time_set[0].start.year != datetime.MINYEAR):
+    dump_data_for_time_slice()
+
+
+for index in range(0, len(time_set)):
+    time_slice = time_set[index]
+
+
+
+get_last_timestamp_for_server(p.netloc)
+#http://mycampus.duapp.com/objsys/api/ufsobj/ufsobj/?format=json
+first_url = "https://%s/objsys/api/ufsobj/ufsobj/?offset=%d&limit=%d&format=json" % (p.netloc, offset, 20)
