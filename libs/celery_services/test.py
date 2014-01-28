c7b019c48521fcd1fc39db370eb946d23717c298
Richard <richardwangwang@gmail.com>
2014-01-28 15:23:37 +0800

    Added simple worker starter.

--- libs/celery_services/test.py
+++ libs/celery_services/test.py
@@ -1,11 +1,17 @@
+import sys
+sys.path.append("D:\\work\\mine\\codes\\ufs_django\\approot")
 from extra_settings.init_settings import init_settings
 init_settings()
-from celery_main import Celery
+from celery import Celery
 
 
+#Ref: http://docs.celeryproject.org/en/master/django/first-steps-with-django.html
+app = Celery('proj')
 
-
-app = Celery('tasks', broker='django://')
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+app.config_from_object('django.conf:settings')
+#app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 @app.task
 def add(x, y):
