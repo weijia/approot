3168b62e9626322b3417fa6acfbb41944f433672
Richard <richardwangwang@gmail.com>
2014-04-27 23:07:25 +0800

    Adding single class diagram use case code.

---
+++ libs/services/simple_app/pull_diagram_in_single_class.py
@@ -0,0 +1,17 @@
+from git_wrapper.puller import Puller
+
+
+class SingleClassDiagramService(object):
+    pass
+
+
+class SimplePullService(SingleClassDiagramService):
+    RUN_EVERY_MIN = 10
+    RUN_ON_TAGS = ["git_repo"]
+
+    def handle_req(self, msg):
+        self.pull(msg["full_path"])
+
+    def pull(self, path):
+        puller = Puller(path)
+        puller.pull_all()
