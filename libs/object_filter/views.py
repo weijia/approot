17ea2440564ac6979b3cdd42b017cea32f70b5ab
Richard <richardwangwang@gmail.com>
2014-05-01 15:40:25 +0800

    Revert filter view from overwritten by SAE codes.

--- libs/object_filter/views.py
+++ libs/object_filter/views.py
@@ -29,18 +29,32 @@ def object_filter(request):
     return render_to_response('object_filter/index.html', c)
 
 
-def object_table(request):
-    data = retrieve_param(request)
-
-    c = {"user": request.user}
-    '''
-    if "tag" in data:
-        c["tag"] = data["tag"]
-        c["filter_tag"] = True
-
-    if "query_base" in data:
-        c["query_base"] = string_tools.unquote_unicode(data["query_base"])
-        c["query_base_exists"] = True
-    '''
+def get_object_table(request, descriptor_class):
+    item_dict_url = descriptor_class().get_object_list_url()
+    item_action_html = descriptor_class().get_object_action_html()
+    c = {"user": request.user, "item_list_url": item_dict_url, "item_actions": item_action_html}
     c.update(csrf(request))
     return render_to_response('object_filter/table.html', c)
+
+
+
+
+
+def object_table(request):
+    try:
+        from object_filter.desccriptors.diagram_descriptor import DiagramDescriptor
+        from object_filter.desccriptors.service_descriptor import ServiceDescriptor
+    except ImportError:
+        pass
+    descriptor_dict = {
+        "diagram": DiagramDescriptor,
+        "service": ServiceDescriptor,
+    }
+
+    data = retrieve_param(request)
+    #item_dict_url = data.get("items_url", "/connection/diagram_list/")
+    descriptor_class = descriptor_dict[data.get("descriptor", "diagram")]
+    return get_object_table(request, descriptor_class)
+
+
+
