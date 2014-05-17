127fd0d24044dbc5da603af9e8b4ff1ea7a582fb
Richard <richardwangwang@gmail.com>
2014-05-17 20:54:57 +0800

    Merging issue fixing codes.

--- libs/webmanager/django_commands_dict.py
+++ libs/webmanager/django_commands_dict.py
@@ -1,14 +1,1 @@
-django_commands_dict = {'clean_nonces': 'social_auth', 'compilemessages': 'django.core', 'dumpdata': 'django.core',
-                        'startproject': 'django.core', 'cleanupregistration': 'registration',
-                        'findstatic': 'django.contrib.staticfiles', 'loaddata': 'django.core',
-                        'createcachetable': 'django.core', 'flush': 'django.core', 'cleanup_dblog': 'djangodblog',
-                        'syncdb': 'django.core', 'sqlinitialdata': 'django.core',
-                        'runserver': 'django.contrib.staticfiles', 'dbshell': 'django.core',
-                        'clean_associations': 'social_auth', 'runfcgi': 'django.core', 'test': 'django.core',
-                        'sqlclear': 'django.core', 'changepassword': 'django.contrib.auth', 'sqlreset': 'django.core',
-                        'shell': 'django.core', 'sqlsequencereset': 'django.core', 'testserver': 'django.core',
-                        'makemessages': 'django.core', 'sql': 'django.core', 'validate': 'django.core',
-                        'sqlall': 'django.core', 'collectstatic': 'django.contrib.staticfiles', 'reset': 'django.core',
-                        'diffsettings': 'django.core', 'collectcmd': 'webmanage', 'inspectdb': 'django.core',
-                        'startapp': 'django.core', 'createsuperuser': 'django.contrib.auth', 'sqlflush': 'django.core',
-                        'sqlcustom': 'django.core', 'cleanup': 'django.core', 'sqlindexes': 'django.core'}
+django_commands_dict = {'clean_nonces': 'social_auth', 'compilemessages': 'django.core', 'dumpdata': 'django.core', 'startproject': 'django.core', 'cleanupregistration': 'registration', 'findstatic': 'django.contrib.staticfiles', 'loaddata': 'django.core', 'createcachetable': 'django.core', 'flush': 'django.core', 'syncdb': 'south', 'convert_to_south': 'south', 'sqlinitialdata': 'django.core', 'migrationcheck': 'south', 'runserver': 'django.contrib.staticfiles', 'clean_orphan_obj_perms': 'guardian', 'dbshell': 'django.core', 'clean_associations': 'social_auth', 'runfcgi': 'django.core', 'test': 'south', 'datamigration': 'south', 'sqlclear': 'django.core', 'changepassword': 'django.contrib.auth', 'sqlreset': 'django.core', 'shell': 'django.core', 'sqlsequencereset': 'django.core', 'testserver': 'south', 'makemessages': 'django.core', 'schemamigration': 'south', 'graphmigrations': 'south', 'startmigration': 'south', 'sql': 'django.core', 'validate': 'django.core', 'sqlall': 'django.core', 'collectstatic': 'django.contrib.staticfiles', 'reset': 'django.core', 'diffsettings': 'django.core', 'collectcmd': 'webmanager', 'inspectdb': 'django.core', 'startapp': 'django.core', 'migrate': 'south', 'createsuperuser': 'django.contrib.auth', 'sqlflush': 'django.core', 'sqlcustom': 'django.core', 'cleanup': 'django.core', 'sqlindexes': 'django.core'}
